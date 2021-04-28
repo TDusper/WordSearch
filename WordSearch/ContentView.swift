934defca47efd5feb056a45c424fdd1b073327db
Toni Dusper <txd2847@g.rit.edu>
2021-04-28 14:40:30 +0200

    Initial Commit

---
+++ WordSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordSearch
+//
+//  Created by user186139 on 4/28/21.
+//  Copyright © 2021 user186139. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
